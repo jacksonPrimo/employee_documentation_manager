93efec7ad39552a818b39abf3595118581f7a6f8
jacksonPrimo <jackson.primodev@gmail.com>
2025-07-23 22:31:19 -0300

    feat: add employee model

---
+++ prisma/migrations/20250723192452_create_employee/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "hiredAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
