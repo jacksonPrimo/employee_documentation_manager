e5e17c037077471e8b76bc7502d343c512c07849
jacksonPrimo <jackson.primodev@gmail.com>
2025-07-29 21:48:42 -0300

    feat: add document-type model

---
+++ prisma/migrations/20250729234947_create_document_type/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "DocumentType" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "DocumentType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DocumentType_name_key" ON "DocumentType"("name");
